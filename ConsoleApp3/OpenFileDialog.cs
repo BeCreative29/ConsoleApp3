176d2050ad589a974059f5de5e4dec8fb3f4e412
МАКС <@DESKTOP-AAP5NDN>
2022-10-16 19:37:09 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3/OpenFileDialog.cs
@@ -0,0 +1,16 @@
+﻿namespace WindowsFormsApplication1
+{
+    internal class OpenFileDialog
+    {
+        public bool Multiselect { get; internal set; }
+        public string DefaultExt { get; internal set; }
+        public string Filter { get; internal set; }
+        public string Title { get; internal set; }
+        public string FileName { get; internal set; }
+
+        internal object ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,83 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public object DialogResult { get; private set; }
+        public object MessageBox { get; private set; }
+        public object MessageBoxButtons { get; private set; }
+        public object MessageBoxIcon { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //поиск файла Excel
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.DefaultExt = "*.xls;*.xlsx";
+            ofd.Filter = "Microsoft Excel (*.xls*)|*.xls*";
+            ofd.Title = "Выберите документ Excel";
+            if (ofd.ShowDialog() != DialogResult.OK)
+            {
+                object value = MessageBox.Show("Вы не выбрали файл для открытия", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            string xlFileName = ofd.FileName; //C:\Users\МАКС\Desktop
+
+            //рабоата с Excel
+            Excel.Range Rng;
+            Excel.Workbook xlWB;
+            Excel.Worksheet xlSht;
+            int iLastRow, iLastCol;
+
+            Excel.Application xlApp = new Excel.Application(); //создаём приложение Excel
+            xlWB = xlApp.Workbooks.Open(xlFileName); //открываем наш файл           
+            xlSht = xlWB.Worksheets["Лист1"]; //или так xlSht = xlWB.ActiveSheet //активный лист
+
+            //iLastRow = xlSht.Cells[xlSht.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row; //последняя заполненная строка в столбце А
+            //iLastCol = xlSht.Cells[1, xlSht.Columns.Count].End[Excel.XlDirection.xlToLeft].Column; //последний заполненный столбец в 1-й строке
+            //Rng = (Excel.Range)xlSht.Range["A1", xlSht.Cells[iLastRow, iLastCol]]; //пример записи диапазона ячеек в переменную Rng
+
+            Rng = xlSht.get_Range("A:A"); //берём весь столбец А в переменную Rng
+            double sum = xlApp.WorksheetFunction.Sum(Rng); //вычисляем сумму ячеек
+            
+            //закрытие Excel
+            xlWB.Close(true); //сохраняем и закрываем файл
+            xlApp.Quit();
+            releaseObject(xlSht);
+            releaseObject(xlWB);
+            releaseObject(xlApp);
+
+            object value1 = MessageBox.Show("Сумма чисел в столбце А равна " + sum, "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+    }
+}
